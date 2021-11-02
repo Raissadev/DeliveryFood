697b73bc78aebcf55ae7f6521df683cb73c165a7
Raissadev <82960240+Raissadev@users.noreply.github.com>
2021-11-02 14:36:41 -0300

    Update payment

--- classes/models/Payment.php
+++ classes/models/Payment.php
@@ -12,10 +12,10 @@
 
     public function __construct(){
       $this->url = 'https://api.stripe.com';
-      $this->key = 'sk_test_suaApiKey';
+      $this->key = 'sk_test_51Jcdy2HUgQFXPmU5PD2UU9FYazEDyZUrSyoX9yCf0AO02hEel5OOEi0UlfZ8lrOB7WuNJCsyV5HaU8rGWSkHkmzv00b1EBQawD';
     }
 
-    public function payIntent($amount){
+    public function payIntent($user_id,$product_id,$payment_id,$card_number,$date_valid,$cvv,$amount){
       $this->endpoint = '/v1/payment_intents';
       $this->data = [
           "amount"               => $amount,
@@ -33,6 +33,7 @@
 
 
     public function post(){
+      echo $this->url;
       $url_format = $this->url.$this->endpoint;
       $key_format = ['api_key' => $this->key];
 
@@ -41,9 +42,18 @@
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($this->data)
     );
-      curl_setopt($ch, CURLOPT_HTTPHEADER, ["Authorization: Bearer sk_test_**"]);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, ["Authorization: Bearer sk_test_51Jcdy2HUgQFXPmU5PD2UU9FYazEDyZUrSyoX9yCf0AO02hEel5OOEi0UlfZ8lrOB7WuNJCsyV5HaU8rGWSkHkmzv00b1EBQawD"]);
       $this->callback = json_decode(curl_exec($ch));
       curl_close($ch);
+      $idReference = $this->callback->id;
+
+      if($this->endpoint == '/v1/payment_intents'){
+        $paymentInsert = \MySql::connect()->prepare("INSERT INTO `payments` VALUES (null,?,?,?,?,?,?,?,?)");
+        $paymentInsert->execute(array($_POST['user_id'], $_POST['product_id'], $idReference, $_POST['card_number'], $_POST['date_valid'], $_POST['cvv'], $_POST['amount'],date("Y-m-d")));  
+        echo "<script> alert('Pagamento efetuado com sucesso!') </script>";
+        echo '<script> location.href = "'.BASE.'" </script>';
+        unset($_SESSION['carrinho']);
+      }
 
     }
 
